2b65304ea48d11b681f8ca795fec85efd1c59746
aniketchavan2211 <aniketchavan2211@gmail.com>
2024-01-13 19:29:47 +0530

    Adding Rust Hello Program

---
+++ application/target/debug/application.d
@@ -0,0 +1,1 @@
+/home/kali/Desktop/Labs/Git/Rust/application/target/debug/application: /home/kali/Desktop/Labs/Git/Rust/application/src/main.rs
---
+++ application/target/debug/deps/application-2eff1e95630d9d16.d
@@ -0,0 +1,5 @@
+/home/kali/Desktop/Labs/Git/Rust/application/target/debug/deps/application-2eff1e95630d9d16: src/main.rs
+
+/home/kali/Desktop/Labs/Git/Rust/application/target/debug/deps/application-2eff1e95630d9d16.d: src/main.rs
+
+src/main.rs:
