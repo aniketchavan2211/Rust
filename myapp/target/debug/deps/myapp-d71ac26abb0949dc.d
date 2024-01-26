b7fc7c252a5a38b790be0cb65b5388be93c85b31
aniketchavan2211 <aniketchavan2211@gmail.com>
2024-01-26 22:12:34 +0530

    nothing

---
+++ myapp/target/debug/deps/myapp-d71ac26abb0949dc.d
@@ -0,0 +1,5 @@
+/home/kali/Desktop/Labs/Git/Languages/Rust/myapp/target/debug/deps/myapp-d71ac26abb0949dc: src/main.rs
+
+/home/kali/Desktop/Labs/Git/Languages/Rust/myapp/target/debug/deps/myapp-d71ac26abb0949dc.d: src/main.rs
+
+src/main.rs:
---
+++ myapp/target/debug/myapp.d
@@ -0,0 +1,1 @@
+/home/kali/Desktop/Labs/Git/Languages/Rust/myapp/target/debug/myapp: /home/kali/Desktop/Labs/Git/Languages/Rust/myapp/src/main.rs
